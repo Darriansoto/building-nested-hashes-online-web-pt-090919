f9c6b197f5233fb19acfaf768dbec79aedac1470
Darrian Soto <ddog350@gmail.com>
2019-10-08 16:49:51 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,9 +5,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        :Benvolio => {
-          
-        }
+        :Benvolio => {}
+        :Mercutio => {}
       ]
    }, 
    :capulet => {
@@ -15,7 +14,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"}
       :heroine_friends => [
-        :Mercutio
+        :Steven => {}
+        :Nurse => {}
       ]
    }
 }
